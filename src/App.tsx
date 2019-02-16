e7bea336ae4dfa447505c475904791808af47479
hodongman <jhd9206@gmail.com>
2019-02-16 16:54:33 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          HAHA
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
